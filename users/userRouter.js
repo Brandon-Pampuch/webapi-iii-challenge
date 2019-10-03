de6caadd598fbcce3e64ad33cad91cb9115ad848
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-10-03 12:30:36 -0500

    add user routes

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,10 +6,30 @@ router.use(express.json())
 
 router.post('/', (req, res) => {
 
+    db.insert(req.body)
+        .then(post => {
+            res.json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "could not post object"
+            })
+        })
 });
 
 router.post('/:id/posts', (req, res) => {
-
+    const { id } = req.params
+    const post = req.body
+    console.log(post, id)
+    postsDb.insert(id, post)
+        .then(post => {
+            res.json(post)
+        })
+        .catch(err => res.status(500).json({
+            error: err,
+            message: "could not post comment"
+        }))
 });
 
 router.get('/', (req, res) => {
@@ -34,18 +54,89 @@ router.get('/', (req, res) => {
 
 
 router.get('/:id', (req, res) => {
+    const { id } = req.params
+    db.getById(id)
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(404).json({
+                    message: "could not find user"
+                })
 
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "failed to retrieve user"
+            })
+        })
 });
 
 router.get('/:id/posts', (req, res) => {
-
+    const { id } = req.params
+    db.getUserPosts(id)
+        .then(posts => {
+            if (posts) {
+                res.json(posts)
+            } else {
+                res.status(404).json({
+                    message: "could not find posts"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "failed to retrieve posts"
+            })
+        })
 });
 
 router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    db.remove(id)
+        .then(post => {
+            if (post) {
+                res.json(post).json({
+                    message: "post has been removed"
+                })
+            } else {
+                res.status(404).json({
+                    message: "could not find post to delete"
+                })
+            }
 
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "failed to delete user"
+            })
+        })
 });
 
 router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const updatedUser = req.body
+    console.log(updatedUser)
+    db.update(id, updatedUser)
+        .then(updated => {
+            if (updated) {
+                res.json(updated)
+            } else {
+                res.status(404).json({
+                    message: "could not find user to update"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "failed to update user"
+            })
+        })
 
 });
 
